42ddeb6997f0aca73122f9ef5ce70447947a4736
Lord-Roman <32844741+Lord-Roman@users.noreply.github.com>
2021-05-12 22:17:09 +0300

    Update random.src.js

--- assets/js/random.src.js
+++ assets/js/random.src.js
@@ -16,103 +16,128 @@ function randomInteger(min, max) {
   return Math.round(rand);
 }
 function roll(){
-	$('.monitor').text(getRandom(key))
-	$('.success__number').text(success);
-	$('.fail__number').text(fail);
-	$('.flawless__number').text(flawless);
+    $('.monitor').html('');
+    let result = getRandom(key);
+    result.forEach(function(item) {
+        let elem = '<span style="color:rgb(255,' + item.color + ');">' + item.value + '</span>';
+        $('.monitor').append(elem);
+        $('.monitor').append(',');
+    })
+    $('.success__number').text(success);
+    $('.fail__number').text(fail);
+    $('.flawless__number').text(flawless);
 }
 function getDice(){
-	return randomInteger(1,10);
+    return randomInteger(1,10);
 }
-function rethrow() {
-	dice = getDice();
-	rez.push(dice);
-	if(dice >= difficult){
-		success++;
-		flawless++;
-	}
-	if(dice == 10){
-		rethrow();
-	}
+function rethrow(color) {
+    dice = getDice();
+    if(color){
+        color = color - 60;   
+    }else{
+        color = 180;
+    }
+    let item = {color: color+','+color, value:dice} 
+    rez.push(item);
+
+    if(dice >= difficult){
+        success++;
+        flawless++;
+    }
+    if(dice == 10){
+        rethrow(color);
+    }
+}
+
+function Charm(dice) {
+    if(charm && (dice == 1)){
+        charm--;
+        let newDice = getDice();
+        dice = Charm(newDice);
+    }
+    return dice;
 }
 
 function getRandom(numberOfDice) {
-	success = 0;
-	fail = 0;
-	flawless = 0;
-	dice = 0;
-	charm = 0;
-	rez = [];
+    success = 0;
+    fail = 0;
+    flawless = 0;
+    dice = 0;
+    charm = 0;
+    rez = [];
 
-	if(numberOfDice > 6){
-		charm++;
-	}
-	if(numberOfDice == 10){
-		charm++;
-	}
 
-	for (var i = 0; i < numberOfDice; i++) {
-		dice = getDice();
 
-		if(dice == 1){
-			if(charm){
-				charm = charm - 1;
-				dice = getDice();
-			}
-		}	
-		rez.push(dice);
+    if(numberOfDice >= 6){
+        charm++;
+    }
+    if(numberOfDice >= 8){
+        charm++;
+    }
+    if(numberOfDice == 10){
+        charm++;
+    }
 
-		if(dice >= difficult){
-			success++;
-			flawless++;
-		}
-		if(dice == 1){
-			fail++;
-			success--;
-		}
-		
-		if(dice == 10){
-			rethrow()
-		}
-	}
-	log(rez);
-	return rez
+    for (var i = 0; i < numberOfDice; i++) {
+        dice = getDice();
+
+        dice = Charm(dice);
+        
+        let item = {color: '255,255', value:dice} 
+        rez.push(item);
+
+        if(dice >= difficult){
+            success++;
+            flawless++;
+        }
+        if(dice == 1){
+            fail++;
+            success--;
+        }
+        
+        if(dice == 10){
+            rethrow()
+        }
+    }
+    log(rez);
+
+    return rez
 }
 $(function() {
-	$(document).keydown(function(e) {
-		if(e.key == 'Shift'){
-			difficult__change = 1;
-		}
-	})
-	$(document).keyup(function(e) {
-		var toRoll = 0;
-		log(e.key);
-		log(e.keyCode);
-		log('difficult__change',difficult__change)
-		if(difficult__change){
-			if(e.keyCode > 48 && e.keyCode <= 57){
-				difficult = e.keyCode - 48;
-			}else if(e.keyCode == 48){
-				difficult = e.keyCode - 38;
-			}
-			$('.difficulty__number').text(difficult);
-		}
-		if(e.key == 'Shift'){
-			difficult__change = 0;
-		}
-		if(e.key >= 1 && e.key <= 9)
-		{
-			key = e.key;
-			toRoll = 1;
-		}else if(e.key == 0){
-			key = 10;
-			toRoll = 1;
-		}
-		if(e.keyCode>95 && e.keyCode<=105){
-			key = (key*1) + 10;
-			toRoll = 1;
-		}
-		log(key);
-		toRoll && roll();
-	})
+    $(document).keydown(function(e) {
+        if(e.key == 'Shift'){
+            difficult__change = 1;
+        }
+    })
+    $(document).keyup(function(e) {
+        var toRoll = 0;
+        log(e.key);
+        log(e.keyCode);
+        log('difficult__change',difficult__change)
+        if(difficult__change){
+            if(e.keyCode > 48 && e.keyCode <= 57){
+                difficult = e.keyCode - 48;
+            }else if(e.keyCode == 48){
+                difficult = e.keyCode - 38;
+            }
+            $('.difficulty__number').text(difficult);
+        }
+        if(e.key == 'Shift'){
+            difficult__change = 0;
+        }
+        if(e.key >= 1 && e.key <= 9)
+        {
+            key = e.key;
+            toRoll = 1;
+        }else if(e.key == 0){
+            key = 10;
+            toRoll = 1;
+        }
+        if(e.keyCode>95 && e.keyCode<=105){
+            key = (key*1) + 10;
+            toRoll = 1;
+        }
+        log(key);
+        toRoll && roll();
+    })
 });
