8c4447cd12efb32c84db5d0f586ab680cbb9745a
Lord-Roman <32844741+Lord-Roman@users.noreply.github.com>
2021-06-07 03:17:35 +0300

    Add files via upload

--- assets/js/random.js
+++ assets/js/random.js
@@ -9,6 +9,8 @@ var charm = 1;
 
 
 var rez = [];
+var wall = [];
+
 var dice;
 
 function randomInteger(min, max) {
@@ -23,12 +25,15 @@ function roll(){
         $('.monitor').append(elem);
         $('.monitor').append(',');
     })
-    if(success<0){
-        if(document.getElementById("img")){
-            document.getElementById("img").style.display = "none"
-        }
-        if(document.getElementById("wall")){
-            document.getElementById("wall").style.display = "block";
+    if(!localStorage.getItem('quit')){
+        if(success < 0){
+            if(document.getElementById("img")){
+                document.getElementById("img").style.display = "none"
+            }
+            if(document.getElementById("wall")){
+                document.getElementById("wall").style.display = "block";
+            }
+            moreTooth();
         }
     }
     $('.success__number').text(success);
@@ -76,7 +81,6 @@ function getRandom(numberOfDice) {
     dice = 0;
     charm = 0;
     rez = [];
-
 
 
     if(numberOfDice >= 7){
@@ -133,21 +137,130 @@ function getRandom(numberOfDice) {
             rethrow()
         }
     }
-    log(rez);
+    // log(rez);
 
     return rez
 }
+
+
+
+function moreTooth(){
+    let tooth = document.createElement('img');
+    tooth.src = 'assets/img/tooth.png';
+    tooth.className = 'tooth';
+    let toothX = (randomInteger(15,500));
+    let toothY = 0;
+    let toothRad = (randomInteger(0,359));
+
+    // let toothRad = 0;
+    // console.log("toothX: ",toothX);
+    
+    tooth.style.right = toothX + 'px';
+    tooth.style.bottom = toothY + 'px';
+    tooth.style.transform = 'rotate('+ toothRad +'deg)';
+
+    let collision = {count:0};
+    for (var i = wall.length - 1; i >= 0; i--) {
+       let element = wall[i];
+    // }
+    // wall.forEach(element => {
+        if(!collision.count && (Math.abs(element.x - toothX) < 30) ){
+            collision.count++;
+
+            collision.x1 = element.x;
+            collision.y1 = element.y;
+
+            collision.pos = element.x > toothX;
+        }else{
+            if( collision.pos && (collision.count == 1) && (((collision.x1 - element.x < 60) && (collision.x1 - element.x > 0))&&( ( element.y - collision.y1 < 30) && ( element.y - collision.y1 >= 0) ) ) ){
+                // log('collision+');
+                collision.count++;
+                collision.x2 = element.x;
+                collision.y2 = element.y;
+            }else if( !collision.pos && (collision.count == 1) && (((element.x - collision.x1 < 60) && (element.x - collision.x1 > 0)) && ( ( element.y - collision.y1 < 30 ) && ( element.y - collision.y1 >= 0) ) )) {
+                // log('collision-');
+                collision.count++;
+                collision.x2 = element.x;
+                collision.y2 = element.y;
+            }
+        }
+    }
+    if(collision.count == 1){
+        // log('collision.pos: ',collision.pos);
+        // log('toothX: ',toothX);
+        // log('collision.x1 :',collision.x1)
+        toothX = collision.x1 + (30 * (collision.pos?(-1):(1) ) );
+        // log('toothX: ',toothX);
+        // toothX = collision.x1 + (30 * collision.pos?(-1):(1)); 
+
+        tooth.style.right = toothX + 'px';
+
+        // toothY = collision.y1 + 30;
+
+        toothY = collision.y1;
+        tooth.style.bottom = toothY + 'px';
+
+    }else if(collision.count >= 1){
+
+        // log("x1: ",collision.x1);
+        // log("x2: ",collision.x2);
+        // log("toothX: ",toothX);
+        // log("pos: ",collision.pos);
+        
+        let min = Math.min(collision.x1,collision.x2);
+        let mid = (Math.abs(collision.x1 - collision.x2))/2
+        
+        // log("min: ",min);
+        // log("mid: ",mid);
+
+        toothX = min + mid;
+        tooth.style.right = toothX + 'px';
+        toothY = collision.y1 + 30 - (mid - 15);
+        tooth.style.bottom = toothY + 'px';
+
+        // toothX = collision.x1 + (30 * (collision.pos?(-1):(1) ) );
+    }
+    wall.push({x: toothX, y:toothY, rad:toothRad})
+    localStorage.setItem('wall', JSON.stringify(wall));
+    document.body.append(tooth);
+}
+
+
 $(function() {
+    if(!localStorage.getItem('quit')){
+
+        let loc = localStorage.getItem('wall')
+        let a = JSON.parse(loc);
+        if(a){
+            wall = a;
+        }
+        wall.forEach(element => {
+            let tooth = document.createElement('img');
+            tooth.src = 'assets/img/tooth.png';
+            tooth.className = 'tooth';
+            
+            tooth.style.right = element.x + 'px';
+            tooth.style.bottom = element.y + 'px';
+            tooth.style.transform = 'rotate('+ element.rad +'deg)';
+
+            document.body.append(tooth);
+        });
+
+    }
+
     $(document).keydown(function(e) {
         if(e.key == 'Shift'){
             difficult__change = 1;
         }
     })
     $(document).keyup(function(e) {
+        
+        
+
         var toRoll = 0;
-        log(e.key);
-        log(e.keyCode);
-        log('difficult__change',difficult__change)
+        // log(e.key);
+        // log(e.keyCode);
+        // log('difficult__change',difficult__change)
         if(difficult__change){
             if(e.keyCode > 48 && e.keyCode <= 57){
                 difficult = e.keyCode - 48;
@@ -163,15 +276,19 @@ $(function() {
         {
             key = e.key;
             toRoll = 1;
-        }else if(e.key == 0){
+        }else if(e.key === 0){
             key = 10;
             toRoll = 1;
+        }else if(e.key == 'Q'){
+            localStorage.setItem('quit', true);
+        }if(e.key == 'C'){
+            localStorage.clear();
         }
         if(e.keyCode>95 && e.keyCode<=105){
             key = (key*1) + 10;
             toRoll = 1;
         }
-        log(key);
+        // log(key);
         toRoll && roll();
     })
 });
