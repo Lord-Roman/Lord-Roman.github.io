e4da4f297085054409c1410b67ed2293439693bf
Роман Королёв <rk89101459876@gmail.com>
2025-08-04 21:32:21 +0300

    add secure

--- you_are_here/app.js
+++ you_are_here/app.js
@@ -51,7 +51,7 @@ function initApp() {
     })
     async function displayCountryByGPS(lat, lng){
         // 
-        const response = await fetch(`http://api.geonames.org/countryCodeJSON?lat=${lat}&lng=${lng}&username=${USER_NAME}`);
+        const response = await fetch(`https://secure.geonames.org/countryCodeJSON?lat=${lat}&lng=${lng}&username=${USER_NAME}`);
             // `https://geocode.xyz/${lat},${lng}?geoit=json&auth=${API_KEY}`
         if(!response.ok) return;
         const { countryName } = await response.json();
